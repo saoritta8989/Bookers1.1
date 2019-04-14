a83028b04c0b07966c438a1f7848108b5016e3c9
saoritta8989 <ymrns133810@gmail.com>
2019-04-14 15:15:47 +0900

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,40 @@
+class BooksController < ApplicationController
+
+  def create
+  	book = Book.new(post_params)
+  	book.save
+  	flash[:notice] = "Book was succesfully"
+    redirect_to book_path(book.id)
+  end
+
+  def index
+  	@books = Book.all
+  	@book = Book.new
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	book = Book.find(params[:id])
+  	book.update(post_params)
+  	redirect_to book_path(book.id)
+  end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+  	redirect_to books_index_path
+  end
+
+  private
+
+  def post_params
+  	 params.require(:book).permit(:title, :body)
+  end
+end
