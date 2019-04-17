581d66004be5c14110c2b48529a987b097a0c217
saoritta8989 <ymrns133810@gmail.com>
2019-04-17 14:32:01 +0900

    Bookers1

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'books/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'top' => 'root#top'
+  root 'root#top'
 
   get 'books' => 'books#index'
 
